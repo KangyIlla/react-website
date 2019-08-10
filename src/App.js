253d76ed76fb8f42e607ccc654609414b3b5792f
Kangeyan Illavalagan <kangeyanillavalagan@1x-193-157-162-200.uio.no>
2019-08-10 13:26:11 +0200

    added lines

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React dad ddqw d
         </a>
       </header>
       <div className="row"> 
