e0de487534f24b8fca6272995c85650dc481cfc9
Kangeyan Illavalagan <kangeyanillavalagan@1x-193-157-250-161.uio.no>
2019-08-10 16:33:49 +0200

    Added card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row' 
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Row>
-          <header>
-            Velkommen til minside
-          </header>
-        </Row>
-        <Row>
-          <Col> 1 av 1</Col>
-          <Col> 2 av 2</Col>
-        </Row>
-      </Container>
+      <Card bg="primary" text="white" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Primary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
